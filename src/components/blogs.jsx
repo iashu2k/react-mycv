81ecfdf515b406b9e27f616a13d1453e6068db9c
iashu2k <iashu2k@gmail.com>
2020-08-04 13:33:07 +0530

    Updated blog

--- src/components/blogs.jsx
+++ src/components/blogs.jsx
@@ -7,7 +7,7 @@ const Blogs = () => {
       title: "Message Traceability vs End-to-End Encryption",
       desc:
         "The craze of texting can be seen in the world, we see people texting anytime and anyplace, these texts connect people to their friends, family and relatives...",
-      img: "https://source.unsplash.com/33oxtOMk6Ac/1600x900",
+      img: "https://images.unsplash.com/photo-1514464750060-00e6e34c8b8c?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1600&h=900&fit=crop&ixid=eyJhcHBfaWQiOjF9",
       date: "June 4",
       url:
         "https://medium.com/@iashu2k/message-traceability-vs-end-to-end-encryption-add848ad7159",
